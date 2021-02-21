77515f5a7c066471da6acbd6703e0feaed7fd6f3
James <carmody_james@hotmail.com>
2021-02-22 08:48:38 +1100

    func

--- dist/index.bs.4992a256.js
+++ dist/index.bs.4992a256.js
@@ -28342,7 +28342,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59387" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60063" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- functions/backend.js
+++ functions/backend.js
@@ -1,4 +1,6 @@
-exports.handler =  async function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return context.logStreamName
+exports.handler = async function(event, context) {
+  return {
+      statusCode: 200,
+      body: JSON.stringify({message: "Hello World"})
+  };
 }
